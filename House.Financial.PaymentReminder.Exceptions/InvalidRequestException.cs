9f8503fc7f6a2d5c6ec7c3b063b150ec1c2596ec
Matheus Segredo <msegredo@onlineapp.com.br>
2021-12-22 08:53:40 -0300

    Lib para exceptions adicionada

---
+++ House.Financial.PaymentReminder.Exceptions/InvalidRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace House.Financial.PaymentReminder.Exceptions
+{
+    public class InvalidRequestException : Exception
+    {        
+        public InvalidRequestException(string message) : base(message)
+        {
+        }
+    }
+}
