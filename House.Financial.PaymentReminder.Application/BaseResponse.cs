46ce395377ac1242956d3f1668e222a2327ec3fc
Matheus Segredo <msegredo@onlineapp.com.br>
2021-12-22 08:53:09 -0300

    Profiles de automapper

---
+++ House.Financial.PaymentReminder.Application/BaseResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace House.Financial.PaymentReminder.Application
+{
+    public class BaseResponse
+    {
+        public BaseResponse(object data, int statusCode = 200)
+        {
+            Data = data;
+            StatusCode = statusCode;
+        }
+
+        public BaseResponse(List<ErrorResponse> errors, int statusCode = 500)
+        {
+            Errors = errors;
+            StatusCode = statusCode;
+        }
+
+        public object? Data { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public List<ErrorResponse> Errors { get; set; }
+    }
+
+    public class ErrorResponse 
+    {
+        public string Message { get; set; }
+
+        public string Property { get; set; }
+    }
+}
---
+++ House.Financial.PaymentReminder.Application/Profiles/Command/PaymentCommandProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using House.Financial.PaymentReminder.Application.Commands;
+using House.Financial.PaymentReminder.Data;
+
+namespace House.Financial.PaymentReminder.Application.Profiles
+{
+    public class PaymentProfile : Profile
+    {
+        public PaymentProfile()
+        {
+            CreateMap<PostPaymentReminderCommand, Payment>();                
+        }
+    }
+}
